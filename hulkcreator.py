41fa322fe48ad42feb8a3a4d82d6e2adee0b5025
prasadpai195 <prasadpai195@gmail.com>
2019-03-18 22:10:00 +0530

    New commit
    Initial version- KSD piece not in place

---
+++ hulkcreator.py
@@ -0,0 +1,162 @@
+'''
+Author: Prasad Pai, Senior Solutions Architect, NA-SD.
+
+The script takes a yaml file as the input and creates the configurations in bulk. The hostnames should be passed in the YAML file, along with the rules.
+
+Disclaimer:
+1) Not to be shared with the customers. Currently for internal consumption only.
+
+'''
+#!/usr/bin/env python
+#Importing the libraries
+import requests
+import json
+from akamai.edgegrid import EdgeGridAuth
+from urllib.parse import urljoin
+from openpyxl import load_workbook
+import re
+import openpyxl, pprint
+import sys
+import argparse
+from datetime import datetime
+import calendar
+import yaml
+from papifunctions import createNewConfig,addHostNames,updateConfigRules,creatCertEnrollment,activateConfig,createCPCodes,getCPCodes,createSecureEdgeHostname,checkCertEnrollment,getDVChallenges
+import time
+# Main Program
+if __name__ == '__main__':
+    print("Loading up!!! We are now reading your input, please give us a moment...")
+    counthosts=0
+    # Step 1: Consume the YAML file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--config", help="yaml file input")
+    args=parser.parse_args()
+    inputfilename=args.config
+    # Extract the account/contract details from the YAML file.
+    yamlhandle=yaml.load(open(inputfilename))
+    contractid = yamlhandle['OnboardConfig']['Account'][0]
+    groupid=yamlhandle['OnboardConfig']['Account'][1]
+    shortcontractid=contractid.split('_')
+    shortcontractid=shortcontractid[1]
+    shortgroupid=groupid.split('_')
+    shortgroupid=shortgroupid[1]
+    productid=yamlhandle['OnboardConfig']['ProductId'][0]
+    # Extract the Hostnames from the YAML file.
+    hostorigincomponent=yamlhandle['OnboardConfig']['Host-Origin']
+    #Extract the email id where notification emails should be sent.
+    emailnotify=yamlhandle['OnboardConfig']['notify'][0]
+    #To create the SSL cert request or not?
+    certdecision = yamlhandle['OnboardCertconfig']['Action']
+    certcn = yamlhandle['OnboardCertconfig']['CN']
+    certaltnames=yamlhandle['OnboardCertconfig']['Altnames']
+    # Let us first put in the cert request, as that takes a longer time...
+    if certdecision == True:
+      print("Certificate creation job in progress. Please note that the normal certification creation and deployment ETA is approximately 4 hours.")
+      result=creatCertEnrollment(shortcontractid,certcn,certaltnames)
+      if "403" not in result.text:
+        print("Looks like the DV SSL Certificate was created successfully. In ~5-10 minutes, we should have the DV challenges ready for you.")
+        # enrollment ID stored in the below variable.
+        enrollmentidcatch=re.search('(.*)\/enrollments\/([0-9]{1,})\/changes\/([0-9]{1,})"(.*)',str(result.text))
+        enrollmentID=enrollmentidcatch.group(2)
+        changeID=enrollmentidcatch.group(3)
+        print("To check the status of your certificate, use this enrollment ID:"+enrollmentID+" and the change ID:"+changeID+"")
+        dcv="FALSE"
+        while dcv == "FALSE":
+          #Waiting for 5 mins for the DV challenges to be ready. 
+          time.sleep(30)
+          #Now, retrieve the DV challenges.
+          retreiveDVchallenges=getDVChallenges(enrollmentID)
+          #print(retreiveDVchallenges)
+          if retreiveDVchallenges == "success":
+            dcv="TRUE";
+          else:
+            print("Looks like the DCV challenges are not ready yet! We will keep checking every 5 minutes.")    
+        print("Please ensure that you complete the DNS validations for LetsEncrypt ASAP!!")
+        user_dcv_actions_completed="FALSE"
+        while user_dcv_actions_completed == "FALSE":
+          dcv_complete_user=input("Have you setup the DNS entries as per the instructions? Press Y if you have: ")
+          if dcv_complete_user == "Y":
+            user_dcv_actions_completed="TRUE";
+            print("Awesome, Thank you!. Now we only have to wait for a couple of hours for the certificate to be deployed.") 
+            certdeploy="FALSE";     
+            while certdeploy=="FALSE":
+              print("Now, the program will keep checking the status of your certificate every 15 mins, until it gets deployed.We will only go forward with the next steps once certificate is deployed on the platform. This activity might take ~3-4 hours. Appreciate your patience here.")
+              #after every 1 hours of wait time , we will check if the cert has been deployed or not.
+              time.sleep(900)
+              enrollmentstatus=checkCertEnrollment(enrollmentID,changeID)
+              #print(enrollmentstatus.text)
+              if "All of your requested changes are complete" not in enrollmentstatus.text:
+                print("Looks like there is some delay in the certificate deployment. Let us check after an hour...")
+              else:
+               certdeploy="TRUE";
+               print("Looks like the certificate is deployed succesfully. Let us now create a brand new Edge Host Name.")
+          else:
+            print("OK, we will check back in 5 minutes")
+      else:
+        print("something went wrong when creating the ceritificate. Please reach out to Akamai. The program will exit now")
+        exit();
+    while counthosts < len(hostorigincomponent):
+        #Extracting the origin hostnames and digital properties for each hostnames
+        host_origin_data = hostorigincomponent[counthosts].split(':')
+        hostname=host_origin_data[0]
+        origin_dns_ip=host_origin_data[1]
+        #origin_dns_ip has origin IP/DNS stored.
+        print("Building the setup for " + hostname + ". Please wait...")
+        counthosts+=1
+        #Create new config API call.
+        result=createNewConfig(productid,hostname,contractid,groupid)
+        if result.status_code != 201:
+            print("Sorry, something went wrong with the configuration creation! Exiting the program now!")
+            #Enable the next line if you want debugging capabilities
+            print(result.json())
+        else:
+            # Extract the property ID after the new config creation is completed
+            outputcatch=re.search('(.*)\/properties\/(.*)\?(.*)',str(result.json()))   ### Property ID stored in the below variable.
+            propertyid=outputcatch.group(2)
+            result=createSecureEdgeHostname(hostname,shortcontractid,shortgroupid,enrollmentID)
+            #Waiting for the EdgeHostName requests to come to the pending state.
+            time.sleep(600)
+            # call the hostname addition PAPI call
+            result=addHostNames(productid,hostname,contractid,groupid,propertyid)
+            if result.status_code != 200:
+                print("Sorry, something went wrong with hostname addition! Exiting the program now!")
+                #Enable the next line if you want debugging capabilities
+                print(result.json())
+                exit()
+            else:
+                #Now that host name addition is successful, lets create the CPCODE.
+                result=createCPCodes(productid,contractid,groupid,hostname)
+                if result.status_code != 201:
+                  print("CPCODE creation error! Using the default CPCODE")
+                  print(result.json())
+                else:
+                  print("CPCODE created successfully.") 
+                  cpcodeapioutput=re.search('(.*)cpc_(.*)\?(.*)',str(result.json()))   ### Extracting the CPCODE
+                  cpcode=int(cpcodeapioutput.group(2))
+                  cpcode_status=getCPCodes(str(cpcode),contractid,groupid)
+                  #computing the CPCODE creation time for JSON input.
+                  ch=re.search('(.*)([0-9]{4}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}\:[0-9]{2}\:[0-9]{2}Z)(.*)',str(cpcode_status.json()))
+                  output_cp_code_time_capture=ch.group(2) 
+                  utc_time = datetime.strptime(output_cp_code_time_capture, "%Y-%m-%dT%H:%M:%SZ")
+                  epoch_time = (utc_time - datetime(1970, 1, 1)).total_seconds()
+                  cpcode_time=int(epoch_time)  
+                  #End of computing the CPCODE creation time for JSON input.
+                  print("CPCODE is ALL SET!!! Lets update the configuration now.") 
+                  result=updateConfigRules(contractid,groupid,propertyid,origin_dns_ip,cpcode,cpcode_time,hostname)
+                  if result.status_code != 200:
+                    print("Sorry, something went wrong with config update! Exiting the program now!")
+                    print(result.json())
+                    exit()
+                  else:
+                    print("Success! Config is now updated!, Activating the configuration to STAGING network now.")
+                    result=activateConfig(contractid,groupid,propertyid,emailnotify)
+                    if result.status_code != 201:
+                        print("There seems to be some error in deploying this configuration,Please check the Luna Portal. Error string below:")
+                        print(result.json())
+                        exit()
+                    else:
+                        print("Successfully deployed the config to staging network. Please give 15 mins for the activation to complete.") 
+
+
+
+
---
+++ papifunctions.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+import requests
+import json
+from akamai.edgegrid import EdgeGridAuth
+from urllib.parse import urljoin
+import re
+import sys
+import argparse
+from datetime import datetime
+import calendar
+import yaml
+
+
+yamlconfighandle=yaml.load(open('credentials.yml'))
+baseurl= yamlconfighandle['Credentials'][0]
+client_token=yamlconfighandle['Credentials'][1]
+client_secret=yamlconfighandle['Credentials'][2]
+access_token=yamlconfighandle['Credentials'][3]
+s = requests.Session()
+s.auth = EdgeGridAuth(
+client_token=client_token,
+client_secret=client_secret,
+access_token=access_token
+)
+def creatCertEnrollment(contractid,commonname,altnames):
+  #Function to create cert enrollment.
+  #Extract the short contracid. Example, if contractid is ctr_G-123GHS, the shortcontractid=G-123GHS
+  headers = {"content-type": "application/vnd.akamai.cps.enrollment.v7+json","Accept":"application/vnd.akamai.cps.enrollment-status.v1+json"}
+  hostname="test.prasadtest.akamaidevops.com"
+  data={'ra': 'lets-encrypt', 'validationType': 'dv', 'certificateType': 'san', 'networkConfiguration': {'geography': 'core', 'secureNetwork': 'enhanced-tls', 'mustHaveCiphers': 'ak-akamai-default', 'preferredCiphers': 'ak-akamai-default', 'sniOnly': True, 'quicEnabled': False, 'disallowedTlsVersions': ['TLSv1', 'TLSv1_1'], 'dnsNameSettings': {'cloneDnsNames': False, 'dnsNames': None}}, 'signatureAlgorithm': 'SHA-256', 'changeManagement': False, 'csr': {'cn': commonname, 'c': 'US', 'st': 'MA', 'l': 'Cambridge', 'o': 'Akamai', 'ou': 'IT', 'sans': altnames}, 'org': {'name': 'Akamai Technologies', 'addressLineOne': '150 Broadway', 'addressLineTwo': None, 'city': 'Cambridge', 'region': 'MA', 'postalCode': '02142', 'country': 'US', 'phone': '617-555-0111'}, 'adminContact': {'firstName': 'Test', 'lastName': 'Test', 'phone': '111-111-1111', 'email': 'test@testxac.com', 'addressLineOne': '150 Broadway', 'addressLineTwo': None, 'city': 'Cambridge', 'country': 'US', 'organizationName': 'Akamai', 'postalCode': '02142', 'region': 'MA', 'title': 'Customer'}, 'techContact': {'firstName': 'Akamai', 'lastName': 'Akamai', 'phone': '111-111-1111', 'email': 'test@akamai.com', 'addressLineOne': '150 Broadway', 'addressLineTwo': None, 'city': 'Cambridge', 'country': 'US', 'organizationName': 'Akamai', 'postalCode': '02142', 'region': 'MA', 'title': 'Script'}, 'enableMultiStackedCertificates': False}
+  result=s.post(urljoin(baseurl,'/cps/v2/enrollments?contractId='+contractid+''), data=json.dumps(data), headers=headers)
+  return result
+def checkCertEnrollment(enrollmentID,changeID):
+  headers = {"Accept":"application/vnd.akamai.cps.change.v2+json"}
+  result=s.get(urljoin(baseurl,'/cps/v2/enrollments/'+enrollmentID+'/changes/'+changeID+''),headers=headers)
+  return result
+def getDVChallenges(enrollmentID):
+  headers = {"Accept":"application/vnd.akamai.cps.dv-history.v1+json"}
+  result=s.get(urljoin(baseurl,'/cps/v2/enrollments/'+enrollmentID+'/dv-history'),headers=headers)
+  data=result.json();
+  #print(data)
+  if json.dumps(data)=="{\"results\": []}":
+    status="fail";
+  else:
+    status="success";
+    dvtree=data["results"];  
+    for domain in dvtree:  
+      for children in domain["domainHistory"]:
+        for validationtype in children["challenges"]:
+          if validationtype["type"] == "dns-01":
+            print("Please create a TXT DNS record pointing "+validationtype["fullPath"]+ " to " +validationtype["responseBody"]+" with a DNS TTL of 60s")
+  return status    
+def createSecureEdgeHostname(hostname,contractid,groupid,enrollmentid):
+  headers={"content-type": "application/x-www-form-urlencoded"}
+  #data={"productId": productid,"domainPrefix": hostname,"domainSuffix": "edgekey.net","secure":"true","ipVersionBehavior": "IPV4","slotNumber": 19,"certEnrollmentId":enrollmentid}
+  data="cnameHostname="+hostname+"&enrollmentId="+enrollmentid+""
+  print(contractid)
+  print(groupid)
+  result=s.post(urljoin(baseurl,'/config-secure-provisioning-service/v1/secure-edge-hosts?contractId='+contractid+'&groupId='+groupid+''), data=data, headers=headers)
+  print(result.json())
+def createCPCodes(productid,contractid,groupid,hostname):
+  headers={"content-type": "application/json"}
+  data={"cpcodeName": hostname ,"productId": productid}
+  result=s.post(urljoin(baseurl,'/papi/v1/cpcodes?contractId='+contractid+'&groupId='+groupid+''), data=json.dumps(data), headers=headers)
+  return result
+def getCPCodes(cpcode,contractid,groupid):
+   result=s.get(urljoin(baseurl,'/papi/v0/cpcodes/cpc_'+cpcode+'?contractId='+contractid+'&groupId='+groupid+''))
+   return result
+def createNewConfig(productid,hostname,contractid,groupid):
+  data={"productId": productid,"propertyName": hostname}
+  headers = {"content-type": "application/json"}
+  result=s.post(urljoin(baseurl,'/papi/v1/properties/?contractId='+contractid+'&groupId='+groupid+''), data=json.dumps(data), headers=headers)
+  return result
+def addHostNames(productid,hostname,contractid,groupid,propertyid):
+  headers={"content-type": "application/json"}
+  #Hardcoding for now.
+  data=[{"cnameTo":""+hostname+".edgekey.net" ,"cnameFrom": hostname,"cnameType": "EDGE_HOSTNAME","secure":"true"}]
+  result=s.put(urljoin(baseurl, '/papi/v1/properties/'+propertyid+'/versions/1/hostnames/?contractId='+ contractid+'&groupId='+groupid+'&validateHostnames=true'),data=json.dumps(data),headers=headers)
+  return result
+def updateConfigRules(contractid,groupid,propertyid,origin_dns_ip,cpcode,cpcode_time,hostname):
+  headers = {"content-type": "application/vnd.akamai.papirules.latest+json"}
+  data={"rules":{"name":"default","children":[{"name":"Content Compression","children":[],"behaviors":[{"name":"gzipResponse","options":{"behavior":"ALWAYS"}}],"criteria":[{"name":"contentType","options":{"matchCaseSensitive":False,"matchOperator":"IS_ONE_OF","matchWildcard":True,"values":["text/*","application/javascript","application/x-javascript","application/x-javascript*","application/json","application/x-json","application/*+json","application/*+xml","application/text","application/vnd.microsoft.icon","application/vnd-ms-fontobject","application/x-font-ttf","application/x-font-opentype","application/x-font-truetype","application/xmlfont/eot","application/xml","font/opentype","font/otf","font/eot","image/svg+xml","image/vnd.microsoft.icon"]}}],"criteriaMustSatisfy":"all"},{"name":"Static Content","children":[],"behaviors":[{"name":"caching","options":{"behavior":"MAX_AGE","mustRevalidate":False,"ttl":"1d"}},{"name":"prefetch","options":{"enabled":False}},{"name":"prefetchable","options":{"enabled":True}}],"criteria":[{"name":"fileExtension","options":{"matchCaseSensitive":False,"matchOperator":"IS_ONE_OF","values":["aif","aiff","au","avi","bin","bmp","cab","carb","cct","cdf","class","css","doc","dcr","dtd","exe","flv","gcf","gff","gif","grv","hdml","hqx","ico","ini","jpeg","jpg","js","mov","mp3","nc","pct","pdf","png","ppc","pws","swa","swf","txt","vbs","w32","wav","wbmp","wml","wmlc","wmls","wmlsc","xsd","zip","pict","tif","tiff","mid","midi","ttf","eot","woff","woff2","otf","svg","svgz","webp","jxr","jar","jp2"]}}],"criteriaMustSatisfy":"all"},{"name":"Dynamic Content","children":[],"behaviors":[{"name":"downstreamCache","options":{"behavior":"TUNNEL_ORIGIN"}}],"criteria":[{"name":"cacheability","options":{"matchOperator":"IS_NOT","value":"CACHEABLE"}}],"criteriaMustSatisfy":"all"},{"name":"Performance","children":[],"behaviors":[{"name":"http2","options":{"enabled":""}},{"name":"allowTransferEncoding","options":{"enabled":True}},{"name":"removeVary","options":{"enabled":True}}],"criteria":[],"criteriaMustSatisfy":"all","comments":"Improves the performance of delivering objects to end users. Behaviors in this rule are applied to all requests as appropriate."}],"behaviors":[{"name":"origin","options":{"cacheKeyHostname":"ORIGIN_HOSTNAME","compress":True,"enableTrueClientIp": True,"trueClientIpHeader": "True-Client-IP","trueClientIpClientSetting": False,"forwardHostHeader":"REQUEST_HOST_HEADER","httpPort":80,"httpsPort":443,"originSni":True,"originType":"CUSTOMER","verificationMode":"PLATFORM_SETTINGS","hostname":origin_dns_ip,"originCertificate":"","ports":""}},{"name":"cpCode","options":{"value":{"id":cpcode,"description":hostname,"products":["Site_Accel"],"createdDate":cpcode_time,"cpCodeLimits":"null","name":hostname}}},{"name":"caching","options":{"behavior":"NO_STORE"}},{"name":"sureRoute","options":{"enabled":True,"forceSslForward":False,"raceStatTtl":"30m","toHostStatus":"INCOMING_HH","type":"PERFORMANCE","testObjectUrl":"/akamai/test/object.html","enableCustomKey":False}},{"name":"tieredDistribution","options":{"enabled":True,"tieredDistributionMap":"CH2"}},{"name":"prefetch","options":{"enabled":True}},{"name":"allowPost","options":{"allowWithoutContentLength":False,"enabled":True}},{"name":"report","options":{"logAcceptLanguage":False,"logCookies":"OFF","logCustomLogField":False,"logHost":False,"logReferer":False,"logUserAgent":True}},{"name":"realUserMonitoring","options":{"enabled":True}}],"options":{"is_secure":False},"variables":[]},"ruleFormat": "v2018-09-12"}
+  #print(json.dumps(data))
+  result=s.put(urljoin(baseurl, '/papi/v1/properties/'+propertyid+'/versions/1/rules/?contractId='+contractid+'&groupId='+groupid+'&validateRules=true'),data=json.dumps(data),headers=headers)
+  #print (result.json())
+  return result
+def activateConfig(contractID,groupID,propertyid,emailnotify):
+  data = {"propertyVersion": 1 ,"network": "STAGING","note": "Initial Activation","notifyEmails": [emailnotify],"acknowledgeAllWarnings":1}
+  headers = {"content-type": "application/json"}
+  result=s.post(urljoin(baseurl,'/papi/v1/properties/'+propertyid+'/activations/?contractId='+contractID+'&groupId='+groupID+''), data=json.dumps(data), headers=headers)
+  return result
+
+
